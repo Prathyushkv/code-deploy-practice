94a9301e5921ad37f3e37f07df71af5ac63fb385
Prathyushkv <kvprathyush99@gmail.com>
2024-09-12 10:24:02 +0530

    this is the first push on 12 thursday

--- index.php
+++ index.php
@@ -3,3 +3,4 @@
 echo '<h1>This is from code pipeline</h1>';
 echo '<h1>This is the second push to the code pipeline</h1>';
 echo '<h1>This is the third push to the code pipeline</h1>';
+echo '<h2>This is the first push of the second day </h2>';
